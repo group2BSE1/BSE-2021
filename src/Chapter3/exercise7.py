b6176acfe1a79e6ee8bbc51851f2b7baf637dceb
group2BSE1 <2020bse043@std.must.ac.ug>
2021-04-05 14:02:05 +0300

    Some changes made

--- src/Chapter3/exercise7.py
+++ src/Chapter3/exercise7.py
@@ -7,7 +7,7 @@ except:
 
 
 if  location=="Kampala":
-    if pay>10000000:
+    if pay>=10000000:
         print("Sure I can work with this.")
     else:
         print("No way!")    
@@ -19,7 +19,7 @@ elif location=="Mbarara":
 elif location=="Space":
     print("Without a doubt, I'll take it.")
 else:
-    if pay>6000000:
+    if pay>=6000000:
         print("Sure I can work with this.")
     else:
         print("No Thanks, I can find something better.")
