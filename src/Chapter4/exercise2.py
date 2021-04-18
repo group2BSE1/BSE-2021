3a6764c7058f8f001668d919bc71e913688f8309
group2BSE1 <2020bse043@std.must.ac.ug>
2021-04-18 08:04:31 +0300

    Changes made in exercise2

--- src/Chapter4/exercise2.py
+++ src/Chapter4/exercise2.py
@@ -1,11 +1,14 @@
 def investment(C,r,n,t):
     p=C*((1+((r)/n))**(t*n))
     return p
-
-C=float(input("Enter initial amount:"))
-r=float(input("Enter the yearly rate:"))
-t=float(input("Enter years till maturation:"))
-n=float(input("Enter compound interest:"))
+try:
+    C=float(input("Enter initial amount:"))
+    r=float(input("Enter the yearly rate:"))
+    t=float(input("Enter years till maturation:"))
+    n=float(input("Enter compound interest:"))
+except:
+    print("Invalid input!!!")
+    exit()
 
 print(f"The final value is {investment(C,r,t,n):,.2f}")
 
