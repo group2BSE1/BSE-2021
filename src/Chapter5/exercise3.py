a22e75d81889a6b6e0ce2480d15ed698d6103acd
group2BSE1 <2020bse043@std.must.ac.ug>
2021-04-19 11:07:02 +0300

    exercise3 30% done

--- src/Chapter5/exercise3.py
+++ src/Chapter5/exercise3.py
@@ -1,3 +1,24 @@
+#creating function that prints purchase price
+def purchase_price(price1):
+    a=price1//1
+    b=price1%1
+    if a==0:
+        payment_due=print(f"Payment due:{b*100:.0f} cents")       
+    else:
+        payment_due=print(f"Payment due:{a:.0f} dollars and {b*100:.0f} cents")
+
+#creating function for menu of deposit selection
+def deposit_menu():
+    print("\nMenu for deposits:\n")
+    print("""  'n' - deposit a nickel
+  'd' - deposit a dime
+  'q' - deposit a quarter
+  'o' - deposit a one dollar bill
+  'f' - deposit a five dollar bill
+  'c' - cancel the purchase
+""")
+    purchase_price(price1)
+
 #initializing money
 nickels=25
 dimes=25
@@ -8,16 +29,17 @@ print("Welcome to the vending machine change maker program")
 print("Change maker initialized.")
 print(f"Stock contains:\n {nickels} nickels\n {dimes} dimes\n {quarters} quarters\n {ones} ones\n {fives} fives")
 
-#requesting for price input
+#main program
 while True:
     try:
+        #requesting for price input
         price=input("Enter the purchase price (xx.xx) or `q' to quit:")
         if price=="q":
             print("Quiting....")
             break
         price1=float(price)
         if (price1>0) and ((price1*100)%5==0):
-            print(price1)
+            deposit_menu()
         else:
             print("Illegal price: Must be a non-negative multiple of 5 cents.")
     except:
